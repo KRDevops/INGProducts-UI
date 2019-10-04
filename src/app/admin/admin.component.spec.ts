688803c60aab78db705ab284bfa4f104448a388e
sukumarsjl <sukusjl@gmail.com>
2019-10-04 15:34:30 +0530

    changes

--- src/app/admin/admin.component.spec.ts
+++ src/app/admin/admin.component.spec.ts
@@ -1,16 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AdminComponent } from './admin.component';
+import { HeadingComponent } from '../core/heading/heading.component';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
+  let mockEnvironment = {
+    production: false,
+    // apiUrl: 'http://13.233.178.107:8090/ing'
+    apiUrl: 'http://10.117.189.108:8090/ing'
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      declarations: [AdminComponent, HeadingComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +27,13 @@ describe('AdminComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fileUploadUrl have url', () => {
+    let fileUploadUrl = `${mockEnvironment.apiUrl}/products`;
+    const fixture = TestBed.createComponent(AdminComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.fileUploadUrl).toBeDefined();
+    expect(app.fileUploadUrl).toEqual(fileUploadUrl);
   });
 });
